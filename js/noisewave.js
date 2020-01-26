f7e79d73ba0fdb2dd7ac79e630076de0c850b51a
il41 <igilbe1@artic.edu>
2020-01-25 19:23:17 -0600

    physical works init

--- js/noisewave.js
+++ js/noisewave.js
@@ -10,7 +10,7 @@ function draw() {
   j+=0.015;
   background(0);
 
-  fill(255);
+  fill(color(0,0,255));
   // We are going to draw a polygon out of the wave points
   beginShape();
 
@@ -18,7 +18,7 @@ function draw() {
   // let xoff = yoff; // Option #2: 1D Noise
 
   // Iterate over horizontal pixels
-  for (let x = 0; x <= width; x += 10) {
+  for (let x = 0; x <= width; x += 1) {
     // Calculate a y value according to noise, map to
 
     // Option #1: 2D Noise
@@ -28,7 +28,8 @@ function draw() {
     // let y = map(noise(xoff), 0, 1, 200,300);
 
     // Set the vertex
-    vertex(x, y+300-(sin(j)*50));
+    vertex(x, y+250-(sin(j*2)*50));
+    vertex(x*1.2, y+300-(sin(j)*50));
     // Increment x dimension for noise
     xoff += 0.05;
   }
